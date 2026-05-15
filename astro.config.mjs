5b6b642401e380573e066ee92b6e93aef9647730
lullaby6 <lucianobrumer5@gmail.com>
2026-05-15 18:56:47 -0300

    bento grid and my block

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+
+
+import react from "@astrojs/react";
+
 
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        port: 3000
-    }
+  server: {
+      port: 3000
+  },
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
